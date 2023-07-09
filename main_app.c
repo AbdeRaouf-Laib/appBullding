0a0932c794d09b2182e7a148e96cf4b416af68a9
AbdeRaouf-Laib <laibabderaouf2004@gmail.com>
2023-07-09 23:15:23 +0100

    code

--- main_app.c
+++ main_app.c
@@ -9,8 +9,9 @@ WINDOW * W__welcome;
 WINDOW * W__about;
 WINDOW * W__vertion_sponsor;
 WINDOW * W__ABOUT;
-WINDOW * W__list;
+WINDOW * W__list_of_using;
 WINDOW * W__F1;
+WINDOW * W__list2_of_using;
 /*Variables are used to define colors*/
 #define BW 1
 #define BLW 2
@@ -33,9 +34,19 @@ WINDOW * W__F1;
 int Fonction__Key__values;
 int Key__values;
 
-    
+//declration of fonction
+void Color(); 
 int print_face();
-void Color();
+void F__vertion_sponsor();
+int Face_About();
+int face_about(int k);
+int face_hello(int *y1,int *x1);
+void face_title(int y1,int x1,int *z1,int *r1);
+void face_list(int z1,int r1);
+int face_F1();
+
+int print_face2();
+int print_list_using();
 
 int main(){
     initscr();
@@ -59,97 +70,8 @@ void Color(){
     init_pair(RB, COLOR_RED, COLOR_BLACK);
 }
 
-//print the vertion and sponsor
-void F__vertion_sponsor(){  
-    W__vertion_sponsor = newwin(4,40,35,120);
-    mvwprintw(W__vertion_sponsor,1,1,"version: 1.00.000.102.H_kakashi R&H");
-    mvwprintw(W__vertion_sponsor,2, 1,"Sponsor: 3moke Uchiha_MADARA");
-    wrefresh(W__vertion_sponsor);
-}
 
-//print the about content 
-int Face_About() {
-        W__ABOUT = newwin(3,40,17,60);
-        wrefresh(W__ABOUT);
-        wprintw(W__ABOUT,"NOT FOUND!!!press 'enter' for return...");
-        wrefresh(W__ABOUT);
-        Fonction__Key__values = getch();
-        if(Fonction__Key__values == enter){
-            clear();
-            refresh();
-            return Fonction__Key__values = 0;
-        }
-        else{
-            clear();
-            refresh();
-            Face_About();
-        }
-}
-
-//print the about window 
-void face_about(int k){
-    W__about = newwin(3,11,1,155);
-    if( k == 1){
-        wbkgd(W__about, COLOR_PAIR(BW));
-    }
-    box(W__about,0,0);
-    wattron(W__about,A_BOLD);
-    mvwprintw(W__about,1,1,"ABOUT (!)");
-    wattroff(W__about,A_BOLD);
-    wrefresh(W__about);
-}
-
-//print welcome
-void face_hello(int *y1,int *x1){
-    int y,x; 
-    W__welcome = newwin(4,12,3,8);
-    wattron(W__welcome,A_BOLD);
-    wprintw(W__welcome,"WELCOME; to,");
-    wattroff(W__welcome,A_BOLD);
-    wrefresh(W__welcome);
-    getmaxyx(W__welcome,y,x);
-    *y1 = y;
-    *x1 = x;
-}
-
-//Calling the logo function from a logo_face lib
-void face_title(int y1,int x1,int *z1,int *r1){
-    ftitle_logo(8,14);
-    int z,r;
-    getyx(W__logo, z, r);
-    *z1 = z;
-    *r1 = r;
-}
-
-//useing list 
-void face_list(int z1,int r1){
-    W__list = newwin(4, 45,z1 + 14, r1 );
-    wprintw(W__list,("!!!....press ('F1') how to use ;\n"));
-    wprintw(W__list,("!!!....press ('ENTER') to continue ;\n"));
-    wprintw(W__list,("!!!....press ('ESC') to exit ;\n"));
-    wrefresh(W__list);
-}
-
-//print f1 content 
-int face_F1(){
-    W__F1 = newwin(3,40,17,60);
-        wrefresh(W__F1);
-        wprintw(W__F1,"NOT FOUND!!!press 'ENTER' for return...");
-        wrefresh(W__F1);
-        Fonction__Key__values = getch();
-        if(Fonction__Key__values == enter){
-            clear();
-            refresh();
-            return Fonction__Key__values = 0;
-        }
-        else{
-            clear();
-            refresh();
-            face_F1();
-        }
-}
-
-//main fonction for faceApp lib
+//face app fonction
 int print_face(){
     dub:
     Key__values = curs_down;
@@ -169,7 +91,7 @@ int print_face(){
         delwin(W__logo);
         delwin(W__about);
         delwin(W__welcome);
-        delwin(W__list);
+        delwin(W__list_of_using);
         delwin(W__vertion_sponsor);
             Key__values = getch();
             if(Key__values == curs_down || Key__values == curs_up)
@@ -195,9 +117,108 @@ int print_face(){
     if(Key__values == 0)
         goto dub;  
     if(Key__values == enter){
-        
+        clear();
+        refresh();
+
     }
     if(Key__values == esc)
         return Key__values;
     
+}
+//print the vertion and sponsor
+void F__vertion_sponsor(){  
+    W__vertion_sponsor = newwin(4,40,35,120);
+    mvwprintw(W__vertion_sponsor,1,1,"version: 1.00.000.102.H_kakashi R&H");
+    mvwprintw(W__vertion_sponsor,2, 1,"Sponsor: 3moke Uchiha_MADARA");
+    wrefresh(W__vertion_sponsor);
+}
+
+//print the about content 
+int Face_About(){
+        W__ABOUT = newwin(3,40,17,60);
+        wrefresh(W__ABOUT);
+        wprintw(W__ABOUT,"NOT FOUND!!!press 'enter' for return...");
+        wrefresh(W__ABOUT);
+        Fonction__Key__values = getch();
+        if(Fonction__Key__values == enter){
+            clear();
+            refresh();
+            return Fonction__Key__values = 0;
+        }
+        else{
+            clear();
+            refresh();
+            Face_About();
+        }
+}
+
+//print the about window 
+int face_about(int k){
+    W__about = newwin(3,11,1,155);
+    if( k == 1){
+        wbkgd(W__about, COLOR_PAIR(BW));
+    }
+    box(W__about,0,0);
+    wattron(W__about,A_BOLD);
+    mvwprintw(W__about,1,1,"ABOUT (!)");
+    wattroff(W__about,A_BOLD);
+    wrefresh(W__about);
+    return 0;
+}
+
+//print welcome
+int face_hello(int *y1,int *x1){
+    int y,x; 
+    W__welcome = newwin(4,12,3,8);
+    wattron(W__welcome,A_BOLD);
+    wprintw(W__welcome,"WELCOME; to,");
+    wattroff(W__welcome,A_BOLD);
+    wrefresh(W__welcome);
+    getmaxyx(W__welcome,y,x);
+    *y1 = y;
+    *x1 = x;
+    return 0;
+}
+
+//Calling the logo function from a logo_face lib
+void face_title(int y1,int x1,int *z1,int *r1){
+    ftitle_logo(8,14);
+    int z,r;
+    getyx(W__logo, z, r);
+    *z1 = z;
+    *r1 = r;
+}
+
+//useing list 
+void face_list(int z1,int r1){
+    W__list_of_using = newwin(4, 45,z1 + 14, r1 );
+    wprintw(W__list_of_using,("!!!....press ('F1') how to use ;\n"));
+    wprintw(W__list_of_using,("!!!....press ('ENTER') to continue ;\n"));
+    wprintw(W__list_of_using,("!!!....press ('ESC') to exit ;\n"));
+    wrefresh(W__list_of_using);
+}
+
+//print f1 content 
+int face_F1(){
+    W__F1 = newwin(3,40,17,60);
+        wrefresh(W__F1);
+        wprintw(W__F1,"NOT FOUND!!!press 'ENTER' for return...");
+        wrefresh(W__F1);
+        Fonction__Key__values = getch();
+        if(Fonction__Key__values == enter){
+            clear();
+            refresh();
+            return Fonction__Key__values = 0;
+        }
+        else{
+            clear();
+            refresh();
+            face_F1();
+        }
+}
+int print_face2(){
+    
+    
+    
+    return 0;
 }
